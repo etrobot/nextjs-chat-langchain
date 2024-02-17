6767684e27082a0d1f16dc1f6042f6a9987ccd98
franklin <etrobot@outlook.com>
2024-02-17 22:22:27 +0800

    update prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -83,19 +83,36 @@ export async function POST(req: NextRequest) {
     new BingSerpAPI(process.env.BINGSERP_API_KEY)
   ];
 
-  const SYSTEM_TEMPLATE = `Respond after accessing one of the tools {tools} with names "{tool_names}" if neccesary:
-{agent_scratchpad} Provide only ONE action per $JSON_BLOB in the format below.
-\`\`\`
-{{
-  "action": $TOOL_NAME,
-  "action_input": $INPUT
-}}
-\`\`\`
-Observation: action result
-... (Thought/Action/Observation once)
-Thought: I know what to respond
-Action: output final answer
-The question is:\n`;
+  const SYSTEM_TEMPLATE = `Respond to the human as helpfully and accurately as possible. You have access to the following tools:
+
+  {tools}
+  
+  Use a json blob to specify a tool by providing an action key (tool name) and an action_input key (tool input).
+  
+  Valid "action" values: "Final Answer" or {tool_names}
+  
+  Provide only ONE action per $JSON_BLOB, as shown:
+  
+  \`\`\`
+  {{
+    "action": $TOOL_NAME,
+    "action_input": $INPUT
+  }}
+  \`\`\`
+  
+  Follow this format:
+  
+  Question: input question to answer
+  Thought: consider previous and subsequent steps
+  Action:
+  \`\`\`
+  $JSON_BLOB
+  \`\`\`
+  Observation: action result
+  ... (repeat Thought/Action/Observation N times)
+  Thought: I know what to respond
+  Action: output final answer.
+  Begin! Reminder to ALWAYS respond with a valid json blob of a single action. Use tools if necessary. Respond directly if appropriate. Format is Action:\`\`\`$JSON_BLOB\`\`\`then Observation`;
   const prompt = ChatPromptTemplate.fromMessages([
     ["system", SYSTEM_TEMPLATE],
     ["human", "{input}"],
