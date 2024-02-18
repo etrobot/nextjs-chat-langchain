9aab04eaa48d976181055c141bcb8d243270c256
Frank Lin <etrobot@outlook.com>
2024-02-18 10:05:29 +0800

    Update route.ts
    move system template to .env

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -83,32 +83,8 @@ export async function POST(req: NextRequest) {
     new BingSerpAPI(process.env.BINGSERP_API_KEY)
   ];
 
-  const SYSTEM_TEMPLATE = `Respond to the human as helpfully and accurately as possible. You have access to the following tools:
-
-  {tools}
+  const SYSTEM_TEMPLATE = process.env.SYSTEM_TEMPLATE
   
-  Use a json blob to specify a tool by providing an action key (tool name) and an action_input key (tool input).
-  
-  Valid "action" values: "Final Answer" or {tool_names}
-  
-  Provide only ONE action per $JSON_BLOB, as shown:
-  
-  \`\`\`
-  {{
-    "action": $TOOL_NAME,
-    "action_input": $INPUT
-  }}
-  \`\`\`
-  
-  Follow this format:
-  
-  Question: input question to answer
-  Thought: {agent_scratchpad}
-  Observation: action result
-  (Thought/Action/Observation ONE times per action)
-  Thought: I know what to respond
-  Action: output final answer.
-  Begin! Reminder to ALWAYS respond with a valid json blob of a single action. Use tools if necessary. Respond directly if appropriate. Format is Action:\`\`\`$JSON_BLOB\`\`\`then Observation`;
   const prompt = ChatPromptTemplate.fromMessages([
     ["system", SYSTEM_TEMPLATE],
     ["human", "{input}"],
